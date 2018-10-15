cf6d1b2b54571d6e1beb03bd498c088eefd0229b
Andrii Kotliarenko <andreykotlaren@gmail.com>
2018-10-15 10:00:51 +0300

    Add requirements.txt and gitignore edit

---
+++ typemaster64.py
@@ -0,0 +1,13 @@
+import sys
+
+test = """
+I am really long string ' with some shitty text 432; 
+helloWorld = 424; 
+input.add(sometimes, but);
+"""
+
+
+if __name__ == '__main__':
+    sys.stdout.flush()
+    sys.stdout.write('Welcome to the TypeMaster64. Get ready for pain in your ass.')
+    sys.stdout.write(test)
