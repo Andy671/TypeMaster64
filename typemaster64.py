78934b63f2e068bcdf71c5d1c9e9fe4e0e6b5b56
Andrii Kotliarenko <andreykotlaren@gmail.com>
2018-10-15 10:08:39 +0300

    VSCode setup

--- typemaster64.py
+++ typemaster64.py
@@ -1,4 +1,5 @@
 import sys
+from pynput import keyboard
 
 test = """
 I am really long string ' with some shitty text 432; 
@@ -9,5 +10,6 @@ input.add(sometimes, but);
 
 if __name__ == '__main__':
     sys.stdout.flush()
-    sys.stdout.write('Welcome to the TypeMaster64. Get ready for pain in your ass.')
+    sys.stdout.write(
+        'Welcome to the TypeMaster64. Get ready for pain in your ass.')
     sys.stdout.write(test)
