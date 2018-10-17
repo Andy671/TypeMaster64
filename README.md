89bbf7b5a4602635abc3ee5283a239f20a524293
Andrii Kotliarenko <andreykotlaren@gmail.com>
2018-10-17 13:25:58 +0300

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TypeMaster64
 Hardcore typing trainer to use when commuting..
+
+## Usage
+
+```
+pip install -r requirements.txt
+```
+
+*Important: Run process as root. It is required for the **pynput** module to work.*
+
+```
+sudo python typemaster64.py
+```
--- typemaster64.py
+++ typemaster64.py
@@ -1,15 +1,64 @@
 import sys
-from pynput import keyboard
+import os
+import time
+import tkinter
+import _thread
 
-test = """
-I am really long string ' with some shitty text 432; 
-helloWorld = 424; 
-input.add(sometimes, but);
-"""
+ERROR_DURATION = 5
+
+target_text = open('target.txt', 'r', encoding='utf-8').read()
+label_text = None
+is_error = False
+
+
+def refreshUi():
+    label_text.set(target_text)
+
+
+def onKeyPressed(event):
+    if is_error:
+        print('It\'s error time. Chill off.')
+        return
+
+    global target_text
+
+    symbol = repr(event.char)
+    symbol = str(symbol)
+    print('Pressed', symbol)  # repr(event.char))
+
+    if event.char == target_text[0]:
+        target_text = target_text[1:]
+    elif event.char == '\r' and target_text[0] == '\n':
+        target_text = target_text[1:]
+    elif event.char != '':
+        handleError()
+
+    refreshUi()
+
+
+def handleError():
+    print('Error')
+    global is_error
+    is_error = True
+    _thread.start_new_thread(continueAfterError,())
+    return
+
+def continueAfterError():
+    global is_error
+    time.sleep(5)
+    is_error = False
+    return
 
 
 if __name__ == '__main__':
-    sys.stdout.flush()
-    sys.stdout.write(
-        'Welcome to the TypeMaster64. Get ready for pain in your ass.')
-    sys.stdout.write(test)
+    window = tkinter.Tk()
+    window.title("TypeMaster64")
+
+    label_text = tkinter.StringVar()
+    refreshUi()
+
+    label = tkinter.Label(window, width=40, height=8,
+                          textvariable=label_text, justify='left', anchor='nw')
+    label.pack()
+    window.bind("<Key>", onKeyPressed)
+    window.mainloop()
